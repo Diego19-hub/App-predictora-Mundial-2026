c74f223e12e48e64afbbe38328e6dc68d95038c6
Diego Ortega <diegoulises1909@gmail.com>
2026-06-17 14:22:26 -0600

    FIX API URL

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
-
 export const api = axios.create({
-    baseURL: API_BASE_URL,
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const loginUser = (data) => api.post("/auth/login", data);
